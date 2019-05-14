8bdd9c14e3f083da58efef7611a7187bf2a36112
RFuentes22 <00062217@uca.edu.sv>
2019-05-13 22:41:22 -0600

    Added data binding

--- app/src/main/java/com/aldana/ejemplo14/ScoreViewModel.kt
+++ app/src/main/java/com/aldana/ejemplo14/ScoreViewModel.kt
@@ -1,8 +1,16 @@
 package com.aldana.ejemplo14
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 
 class ScoreViewModel : ViewModel() {
     var scoreTeamA = 0
     var scoreTeamB = 0
+
+    val currentvalueTeamA: Int
+        get() = scoreTeamA
+
+    val currentvalueTeamB: Int
+        get() = scoreTeamB
+
 }
